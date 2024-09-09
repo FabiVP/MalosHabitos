f2043fc395f0b10e8af1d7e5206157b24d4112f7
72947169 <72947169@continental.edu.pe>
2024-09-09 15:36:55 -0500

    Modificacion ejercicio 03

--- Ejemploo3.py
+++ Ejemploo3.py
@@ -1,23 +1,22 @@
 # Función para calcular el área de un rectángulo
-def f(a, b):
-    result = a * b
+def f(alto, ancho):
+    result = alto * ancho
     return result
 
 # Función para calcular el área de un triángulo
-def g(b, h):
-    r = 0.5 * b * h
+def g(base, altura):
+    r = 0.5 * base * altura
     return r
 
 # Función principal
-def main():
-    x = 4
-    y = 6
-    rect_area = f(x, y)
-    print("Área del rectángulo:", rect_area)
+if __name__=="__main__":
+    alto= float(input("alto: "))
+    ancho = float(input("ancho: "))
+    rect_area = f(alto, ancho)
+    print(f"{"Área del rectángulo : "} {alto}*{ancho}= {rect_area}")
 
-    base = 5
-    altura = 8
+    base = float(input("base: "))
+    altura = float(input("altura: "))
     tri_area = g(base, altura)
-    print("Área del triángulo:", tri_area)
+    print(f"{"Área del triángulo: : "} {base}*{altura}*{0.5}= {tri_area}")
 
-main()
