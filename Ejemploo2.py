3691c99d646cc55375e75b4f470377c522c705fa
72947169 <72947169@continental.edu.pe>
2024-09-09 15:27:18 -0500

    Modificacion ejercicio 02

--- Ejemploo2.py
+++ Ejemploo2.py
@@ -2,12 +2,11 @@ def calcular(factor1, factor2, factor3):
     operacion = factor1 * factor2 + factor3
     return operacion
 
-def principal():
+if __name__=="__main__":
     operador1 = float(input("Operador 1: "))
     operador2 = float(input("Operador 2: "))
     operador3 = float(input("Operador 3: "))
 
     resultado = calcular(operador1, operador2, operador3)
-    print("El resultado es:", resultado)
+    print(f"{"El resultado es : "} {operador1}*{operador2}+{operador3}= {resultado}")
 
-principal()
